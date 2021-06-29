6f54d8e443df374590e894cdd03253c1b501cb39
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-06-29 22:47:42 +1000

    Add test cases and update the name format

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -2,10 +2,11 @@ import Validator from '../basic';
 
 test('validateUsername method exists and works correctly', () => {
   expect(Boolean(Validator.validateUsername)).toBeTruthy();
-  expect(Validator.validateUsername('abc123abc')).toBeTruthy();
-  expect(Validator.validateUsername('-abc123abc-')).toBeFalsy();
-  expect(Validator.validateUsername('_abc123abc_')).toBeFalsy();
-  expect(Validator.validateUsername('1abc123abc1')).toBeFalsy();
-  expect(Validator.validateUsername('abc1234abc')).toBeFalsy();
-  expect(Validator.validateUsername('a.*+?^${}()|[]\\b')).toBeFalsy();
+  expect(Validator.validateUsername('aBc123-456xYz')).toBeTruthy();
+  expect(Validator.validateUsername('gIj78_90uVw')).toBeTruthy();
+  expect(Validator.validateUsername('-name-')).toBeFalsy();
+  expect(Validator.validateUsername('_name_')).toBeFalsy();
+  expect(Validator.validateUsername('1name1')).toBeFalsy();
+  expect(Validator.validateUsername('name1234')).toBeFalsy();
+  expect(Validator.validateUsername('na.me')).toBeFalsy();
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,6 +1,6 @@
 export default class Validator {
   static validateUsername(name) {
-    const nameFormat = /^[a-z][\w-]+[a-z]$/i;
+    const nameFormat = /^[a-z][\w-]*[a-z]$/i;
     const maxDigits = /\d{4,}/;
     return nameFormat.test(name) && !maxDigits.test(name);
   }
